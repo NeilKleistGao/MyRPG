4802dcfd1ec60cac0b3bf635b01d7381d0abaa67
NeilKleistGao <neilkleistgao@163.com>
2021-09-15 21:53:29 +0800

    add occlusion

---
+++ Assets/AssetsPack/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour {
+    private NavMeshAgent agent;
+    private Animator animator;
+
+    private void Awake() {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Start() {
+        MouseManager.instance.onMouseClicked += MoveToTarget;
+    }
+
+    private void Update() {
+        SwitchAnimation();
+    }
+
+    private void SwitchAnimation() {
+        animator.SetFloat("Speed", agent.velocity.sqrMagnitude);
+    }
+
+    private void MoveToTarget(Vector3 target) {
+        agent.destination = target;
+    }
+}
---
+++ Assets/AssetsPack/Scripts/Managers/MouseManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MouseManager : MonoBehaviour {
+
+    public static MouseManager instance;
+
+    private RaycastHit hitInfo;
+    public event Action<Vector3> onMouseClicked;
+
+    public Texture2D point, doorway, attack, target, arrow;
+
+    private void Awake() {
+        if (instance != null) {
+            Destroy(instance);
+        }
+
+        instance = this;
+    }
+
+    private void Update() {
+        SetCursorTexture();
+        ControlMouse();
+    }
+
+    private void SetCursorTexture() {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hitInfo)) {
+            switch (hitInfo.collider.gameObject.tag) {
+                case "Ground":
+                    Cursor.SetCursor(target, new Vector2(16, 16), CursorMode.Auto);
+                    break;
+            }
+        }
+    }
+
+    private void ControlMouse() {
+        if (Input.GetMouseButtonDown(0) && hitInfo.collider != null) {
+            if (hitInfo.collider.gameObject.CompareTag("Ground")) {
+                onMouseClicked?.Invoke(hitInfo.point);
+            }
+        }
+    }
+}
